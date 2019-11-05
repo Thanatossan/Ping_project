9e31ebd7d80739f4a5806dd06dfc0879d2db8cfe
Thanatossan <kotokikung@gmail.com>
2019-11-05 23:10:02 +0700

    first commit

---
+++ components/Logo.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <b-row class="text-center">
+      <b-col>1 of 3</b-col>
+      <b-col cols="8">
+        <b-button> login </b-button>
+      </b-col>
+      <b-col>3 of 3</b-col>
+    </b-row>
+  </div>
+</template>
+
+<style>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="container grid center">
+    <img
+      src="~/assets/lock.png"
+      class="center"
+      width="50px"
+    >
+    <b-form
+      @submit="onSubmit"
+      v-if="show"
+    >
+      <b-form-group
+        id="input-group-1"
+        label="Username"
+        label-for="input-1"
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.name"
+          type="text"
+          required
+          placeholder="Username"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group
+        id="input-group-2"
+        label="Password"
+        label-for="input-2"
+      >
+        <b-form-input
+          id="input-2"
+          v-model="form.password"
+          type="password"
+          required
+          placeholder="Enter password"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-button
+        type="submit"
+        variant="dark"
+      >Submit</b-button>
+    </b-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        name: "",
+        password: ""
+      },
+      show: true
+    };
+  },
+  methods: {
+    onSubmit(evt) {
+      evt.preventDefault();
+      alert(JSON.stringify(this.form));
+    },
+    onReset(evt) {
+      evt.preventDefault();
+      // Reset our form values
+      this.form.name = "";
+      this.form.name = "";
+      this.form.checked = [];
+      // Trick to reset/clear native browser form validation state
+      this.show = false;
+      this.$nextTick(() => {
+        this.show = true;
+      });
+    }
+  }
+};
+</script>
+<style>
+.grid {
+  display: grid;
+  grid-template-columns: [xl-start] 1fr 1.5rem [md-start] minmax(0, 624px) [md-end] 1.5rem 1fr [xl-end];
+}
+
+.grid * {
+  grid-column: md;
+}
+.grid-xl {
+  grid-column: xl;
+}
+.center {
+  margin: auto;
+  width: 50%;
+  padding: 10px;
+}
+</style>
